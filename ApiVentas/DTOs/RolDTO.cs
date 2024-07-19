ced1feeca06ffe4b228b5f10f5408e32129254fd
Dereck Estrada Molina <dereckdem17@gmail.com>
2024-07-19 17:29:55 -0500

    Metodos CRUD 19072024

---
+++ ApiVentas/DTOs/RolDTO.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Linq.Expressions;
+
+namespace ApiVentas.DTOs
+{
+    public class RolDTO
+    {
+        public int RolId { get; set; }
+        public string? RolDescripcion { get; set; }
+        public short? Estado { get; set; }
+        public DateTime? FechaHoraReg { get; set; }
+        public DateTime? FechaHoraAct { get; set; }
+        public int? UsuIdReg { get; set; }
+        public string? UsuRegDescrip { get; set; }
+        public string? UsuIdAct { get; set; }
+        public Expression<Func<RolDTO, bool>>? DictionaryRol(string? opcion, string? data)
+        {
+            bool validarOpcion = opcion.IsNullOrEmpty();
+            bool validarData = data.IsNullOrEmpty();
+            int.TryParse(data, out int in1);
+            Expression<Func<RolDTO, bool>> query;
+            if (validarOpcion)
+            {
+                query = x => x.Estado == 1;
+            }
+            else
+            {
+                var DictionaryRol= new Dictionary<string, Expression<Func<RolDTO, bool>>>();
+                DictionaryRol.Add("id", x => x.RolId== in1 && x.Estado == 1);
+                DictionaryRol.Add("descripcion", x => x.RolDescripcion.ToLower().Equals(data.ToLower()) && x.Estado == 1);
+                DictionaryRol.Add("estado", x => x.Estado == in1);
+                query = !validarOpcion ? DictionaryRol.ContainsKey(opcion.ToLower()) && !validarData ? DictionaryRol[opcion.ToLower()] : null : null;
+            }
+            return query;
+        }
+    }
+}
---
+++ ApiVentas/Utilitarios/ControlError.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace ApiVentas.Utilitarios
+{
+    public class ControlError
+    {
+        private readonly IConfiguration _configuration;
+
+        public ControlError()
+        {
+        }
+
+        public ControlError(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+        public void LogError(string clase, string metodo, string message)
+        {
+
+            var ruta=string.Empty;
+            var archivo=string.Empty;
+            DateTime date=DateTime.Now; 
+            try
+            {
+                ruta = _configuration.GetSection("Rutas:CarpetaLog").Value;
+                archivo = $"Log_{date.ToString("dd-MM-yyyy")}";
+                if (!Directory.Exists(ruta))
+                {
+                    Directory.CreateDirectory(ruta);
+                }
+                StreamWriter writer=new StreamWriter($"{ruta}{archivo}", true);
+                writer.WriteLine($"Se presento una novedad en la clase: '{clase}', en el metodo: '{metodo}', con el siguente error: '{message}'");
+                writer.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
