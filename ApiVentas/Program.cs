b26ec4616a993d7dbd4de7dc42f11d9c6f2d7b08
Dereck Estrada Molina <dereckdem17@gmail.com>
2024-07-20 09:49:57 -0500

    Program Scopped

--- ApiVentas/Program.cs
+++ ApiVentas/Program.cs
@@ -1,4 +1,6 @@
+using ApiVentas.Interfaces;
 using ApiVentas.Models;
+using ApiVentas.Services;
 using ApiVentas.Utilitarios;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,7 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddServices();
+builder.Services.AddScoped<IBodegaServices, BodegaServices>();
+builder.Services.AddScoped<IProductoServices, ProductoServices>();
+builder.Services.AddScoped<IProveedorServices, ProveedorServices>();
+builder.Services.AddScoped<IPuntoEmisionSriServices, PuntoEmisionSriServices>();
+builder.Services.AddScoped<IPuntoVentaServices, PuntoVentaServices>();
+builder.Services.AddScoped<IRolServices, RolServices>();
+builder.Services.AddScoped<IStockServices, StockServices>();
+
 builder.Services.AddDbContext<BaseErpContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionDefault")));
 var app = builder.Build();
 
